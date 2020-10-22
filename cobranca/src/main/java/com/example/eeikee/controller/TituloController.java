5ed5782bd73947bb1ffe4612091cfbd9ed2bc0a8
rekm <rekm@gft.com>
2020-10-22 16:00:10 -0300

    add delete and update method

--- cobranca/src/main/java/com/example/eeikee/controller/TituloController.java
+++ cobranca/src/main/java/com/example/eeikee/controller/TituloController.java
@@ -4,10 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.AfterDomainEventPublication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,16 +21,20 @@ import com.example.eeikee.model.StatusTitulo;
 import com.example.eeikee.model.Titulo;
 import com.example.eeikee.repository.Titulos;
 
+import jdk.jshell.ErroneousSnippet;
+
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
 
+	private static final String CADASTRO_VIEW = "CadastroDeTitulo";
+	
 	@Autowired
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
 	public ModelAndView novoTitulo() {
-		ModelAndView mView = new ModelAndView("CadastroDeTitulo");
+		ModelAndView mView = new ModelAndView(CADASTRO_VIEW);
 		mView.addObject(new Titulo());
 		return mView;
 	}
@@ -35,11 +42,16 @@ public class TituloController {
 	@RequestMapping(method = RequestMethod.POST)
 	public String salvar(@Validated Titulo titulo, Errors error, RedirectAttributes redirectAttributes) {
 		if (error.hasErrors()) {
-			return "CadastroDeTitulo";
+			return CADASTRO_VIEW;
 		}
-		titulos.save(titulo);
-		redirectAttributes.addFlashAttribute("mensagem", "Titulo salvo com sucesso!");
-		return "redirect:/titulos/novo";
+		try {
+			titulos.save(titulo);
+			redirectAttributes.addFlashAttribute("mensagem", "Titulo salvo com sucesso!");
+			return "redirect:/titulos/novo";
+		} catch (DataIntegrityViolationException e) {
+			error.rejectValue("dataVencimento", null, "Formato de data invalido");
+			return CADASTRO_VIEW;
+		}	
 	}
 
 	@RequestMapping
@@ -48,6 +60,20 @@ public class TituloController {
 		List<Titulo> todosTitulos = titulos.findAll();
 		mView.addObject("titulos", todosTitulos);
 		return mView;
+	}
+	
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		ModelAndView mView = new ModelAndView(CADASTRO_VIEW);
+		mView.addObject(titulo);
+		return mView;
+	}
+	
+	@RequestMapping(value="{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes) {
+	titulos.deleteById(codigo);
+	attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
+	return "redirect:/titulos";
 	}
 
 	@ModelAttribute("todosStatusTitulo")
